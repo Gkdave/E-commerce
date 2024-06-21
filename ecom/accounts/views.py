2e43de800c157f5fc9dd92296dc3268bb9cbc3e5
gajendra <davegk59@gmil.com>
2024-06-21 18:14:55 +0530

    email and login conf

--- ecom/accounts/views.py
+++ ecom/accounts/views.py
@@ -1,4 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect 
+from django.contrib import messages
+from django.contrib.auth.models import User 
+from django.contrib.auth import authenticate,login,logout 
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Profile 
+
 
 def login_page(request):
-    return render(request,'accounts/login.html')
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user_obj = User.objects.filter(username=email)
+        
+        if not user_obj.exists():
+            messages.warning(request,'Account not found!')
+            return HttpResponseRedirect(request.path_info)
+        
+        if not user_obj[0].profile.is_email_verified:
+            messages.warning(request,'Your Account is not verified.')
+            return HttpResponseRedirect(request.path_info)
+        
+        user_obj = authenticate(username=email,password=password)
+        print(email)
+        if user_obj:
+            login(request,user_obj)
+            return redirect("/accounts/register/")
+        
+        messages.warning(request,'Invalid Credentials')
+        return HttpResponseRedirect(request.path_info)
+        
+    return render(request,'/')
+
+
+def register_page(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name') 
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        #print(request.POST)
+        user_obj = User.objects.filter(username=email)
+        if user_obj .exists():
+            messages.warning(request,"Email is already taken !")
+            return HttpResponseRedirect(request.path_info)
+        #print(email)
+        user_obj = User.objects.create(first_name=first_name,last_name=last_name,email=email,username=email,password=password)
+        user_obj.set_password(password)
+        user_obj.save()
+        messages.success(request,'An email has been sent on your Email')
+        return HttpResponseRedirect(request.path_info)
+    return render(request,'accounts/register.html')
+
+
+
